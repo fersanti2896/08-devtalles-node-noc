1864c75dd1ae8647755ba6ec2374577648bddfd2
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-08 16:52:30 -0600

    Inyección de dependencias.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -8,7 +8,13 @@ export class Server {
         CronService.createJob( 
             '*/5 * * * * *',
             () => {
-                new CheckService().execute('https://google.com');
+                const url = 'https://google.com';
+
+                new CheckService(
+                    () => console.log(`${ url } is ok`),
+                    ( error ) => console.log(error)
+                ).execute( url );
+                // new CheckService().execute('http://localhost:3000');
             }
         );
     }   
