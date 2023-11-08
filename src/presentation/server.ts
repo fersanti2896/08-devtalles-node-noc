d589a2dc170e4f9afb93088e5212aa4b3e14c7ba
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-08 16:36:19 -0600

    Implementando caso de uso CheckService.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import { CronService } from './cron/cron-service';
 
 export class Server {
@@ -7,8 +8,7 @@ export class Server {
         CronService.createJob( 
             '*/5 * * * * *',
             () => {
-                const date = new Date();
-                console.log('5 seconds', date);
+                new CheckService().execute('https://google.com');
             }
         );
     }   
